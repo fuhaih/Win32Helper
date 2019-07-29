037b5163efa8f2dbc854f669543ca3c3258e086b
fuhaih <1154590036@qq.com>
2019-07-29 10:24:17 +0800

    add method DisableQuickEditMode

--- Win32Helper/TrayHelper.cs
+++ Win32Helper/TrayHelper.cs
@@ -96,6 +96,28 @@ namespace Win32Helper
         static extern bool ShowWindow(IntPtr hWnd, int type);
         #endregion
 
+        #region 关闭控制台 快速编辑模式、插入模式
+        const int STD_INPUT_HANDLE = -10;
+        const uint ENABLE_QUICK_EDIT_MODE = 0x0040;
+        const uint ENABLE_INSERT_MODE = 0x0020;
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern IntPtr GetStdHandle(int hConsoleHandle);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint mode);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint mode);
+
+        public static void DisbleQuickEditMode()
+        {
+            IntPtr hStdin = GetStdHandle(STD_INPUT_HANDLE);
+            uint mode;
+            GetConsoleMode(hStdin, out mode);
+            mode &= ~ENABLE_QUICK_EDIT_MODE;//移除快速编辑模式
+            mode &= ~ENABLE_INSERT_MODE;      //移除插入模式
+            SetConsoleMode(hStdin, mode);
+        }
+        #endregion
+
         public static void DisableCloseButton()
         {
             //线程睡眠，确保closebtn中能够正常FindWindow，否则有时会Find失败。。
